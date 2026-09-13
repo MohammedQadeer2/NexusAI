a079c0f8fc7b6f6bd7fbec848a592eeecd64d36b
mohammed Qadeer <mr0060195@gmail.com>
2026-09-13 14:53:24 +0530

    some tesing..

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const allowedOrigins = [
     process.env.CLIENT_URL,
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://nexusai-31dk.onrender.com/"
+    "https://nexusai-31dk.onrender.com"
 ].filter(Boolean);
 
 app.use(cors({
